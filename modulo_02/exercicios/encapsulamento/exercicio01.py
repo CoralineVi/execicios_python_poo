510b7b618bef97b0b5b74974b2030b173b3196ca
Coraline Vitória <coraline.vitoria.cora@gmail.com>
2023-05-27 02:04:00 -0300

    Update exercicio01.py

--- modulo_02/exercicios/encapsulamento/exercicio01.py
+++ modulo_02/exercicios/encapsulamento/exercicio01.py
@@ -2,8 +2,6 @@
 Crie uma classe chamada Pessoa com os atributos nome e idade. Utilize o encapsulamento para
 definir esses atributos como privados e crie métodos para acessá-los (getters) e modificá-los (setters).
 """
-import time
-
 class Pessoa:
     def __init__(self, nome, idade):
         self.__nome = nome
