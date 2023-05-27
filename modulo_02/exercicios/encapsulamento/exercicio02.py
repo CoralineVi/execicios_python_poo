9a1783d3f4b5efa7a45ce7e921da8be4e63ca974
Coraline Vitória <coraline.vitoria.cora@gmail.com>
2023-05-27 02:03:12 -0300

    Exercicios encapsulamento
    exercicios de encapsulamento finalizados.

--- modulo_02/exercicios/encapsulamento/exercicio02.py
+++ modulo_02/exercicios/encapsulamento/exercicio02.py
@@ -3,3 +3,35 @@ Implemente uma classe ContaBancaria com os atributos saldo e limite.
 Utilize o encapsulamentopara tornar esses atributos privados e forneça métodos para depositar e sacar dinheiro da conta, 
 respeitando o limite.
 """
+
+class ContaBancaria:
+    def __init__(self, saldo, limite):
+        self.__saldo = saldo
+        self.__limite = limite
+        pass
+    
+    def setter_saque(self, valor):
+        if valor < self.__saldo:
+            self.__saldo -= valor
+            print(f"Saque de R${valor} realizado com sucesso")
+        else:
+            print("Valor indisponível para o saque, verifique o seu saldo bancário")
+    
+    def setter_deposito(self, valor):
+        if (self.__saldo + valor < self.__limite):
+            self.__saldo += valor
+            print(f"Deposito de R${valor} realizado com sucesso")
+        else:
+            print("Limite da conta atingido!!")
+        return
+    
+    def getter_info(self):
+        print(f"Saldo: {self.__saldo} | Limite: {self.__limite}")
+        pass
+    
+
+conta1 = ContaBancaria(1000, 2000)
+
+conta1.getter_info()
+conta1.setter_deposito(1000)
+conta1.getter_info()
--- modulo_02/exercicios/encapsulamento/exercicio03.py
+++ modulo_02/exercicios/encapsulamento/exercicio03.py
@@ -2,3 +2,26 @@
 Crie uma classe Produto com os atributos nome, preco e desconto. 
 Utilize o encapsulamento para tornar o atributo desconto privado e crie um método para aplicar o desconto ao preço do produto.
 """
+
+class Produto:
+    def __init__(self, nome, preco, desconto):
+        self.nome = nome
+        self.preco = preco
+        self.__desconto = desconto
+        pass
+    
+    def getter_info(self):
+        print(f"""Nome do produto: {self.nome}
+Preço do produto: R${self.preco}""")
+        pass
+    
+    def setter_desconto(self):
+        self.preco -= self.__desconto
+        print(f"Desconto de {self.__desconto} aplicado {self.nome}!!")
+        pass
+    
+camisa = Produto("Camisa", 80, 20)
+
+camisa.getter_info()
+camisa.setter_desconto()
+camisa.getter_info()
