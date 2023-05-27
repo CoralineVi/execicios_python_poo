b8e095853533b2974ac9ec5bd646abb8cd389726
Coraline Vitória <coraline.vitoria.cora@gmail.com>
2023-05-27 13:53:26 -0300

    exercicio01 concluido

--- modulo_02/exercicios/heranca/exercicio01.py
+++ modulo_02/exercicios/heranca/exercicio01.py
@@ -3,3 +3,25 @@ Crie uma classe base chamada Animal com o método fazer_som().
 Em seguida, crie duas classes derivadas, como Cachorro e Gato, que herdam de Animal
 e implementam o método fazer_som() com sons diferentes para cada classe.
 """
+
+class Animal:
+    def __init__(self, nome):
+        self.nome = nome
+        pass
+
+
+class Cachorro(Animal):
+    def fazer_som(self):
+        print(f"O cachorro {self.nome} latiu")
+        pass
+
+class Gato(Animal):
+    def fazer_som(self):
+        print(f"O gato {self.nome} miou")
+        pass
+
+gato_1 = Gato("Nya")
+cachorro_1 = Cachorro("Rex")
+
+gato_1.fazer_som()
+cachorro_1.fazer_som()
